3ff01625c877416e4d0c75897478cf3219363e0a
alex Campbell <alexbe.campbell@gmail.com>
2022-04-25 23:34:18 -0500

    practice get elements by class and tag name

--- app.js
+++ app.js
@@ -76,13 +76,33 @@
 
 //get element by className
 
-const items = document.getElementsByClassName('collection-item')
+// const items = document.getElementsByClassName('collection-item')
 
-console.log(items);
-console.log(items[0]);
-items[0].style.color = 'red';
-items[3].textContent = 'Hello';
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = 'red';
+// items[3].textContent = 'Hello';
 
-const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
 
-console.log(listItems)
+// console.log(listItems)
+
+//Get elements by tag name document.getElementsByTagName()
+
+// let lis = document.getElementsByTagName('li')
+// console.log(lis);
+// console.log(lis[0]);
+// lis[0].style.color = 'red';
+// lis[3].textContent = 'Hello';
+
+// //convert HTML collection into an array
+
+// lis = Array.from(lis);
+
+// lis.reverse()
+// lis.forEach(function(li) {
+//     console.log(li.className)
+//     li.textContent = `${index}: Hello`
+// })
+
+// console.log(lis)
