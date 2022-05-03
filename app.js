1898a2b924f956b39b73c64feceb679e336362d0
alex Campbell <alexbe.campbell@gmail.com>
2022-05-03 00:14:01 -0500

    create element practice and append to UI

--- app.js
+++ app.js
@@ -132,32 +132,93 @@
 
 //DOM traversal
 
-let val;
+// let val;
 
-const list = document.querySelector('ul.collection');
+// const list = document.querySelector('ul.collection');
 
-const listItem = document.querySelector('li.collection-item:first-child');
+// const listItem = document.querySelector('li.collection-item:first-child');
 
-val = listItem;
-val = list;
+// val = listItem;
+// val = list;
 
-//Get child nodes
+// //Get child nodes
 
-val = list.childNodes;
-val = list.childNodes[0];
-val = list.childNodes[0].nodeName;
-val = list.childNodes[3].nodeType;
+// val = list.childNodes;
+// val = list.childNodes[0];
+// val = list.childNodes[0].nodeName;
+// val = list.childNodes[3].nodeType;
 
-//1- Element
-//2- Attribute(DEP)
-//3- Text Node
-//8- Comment
-//9- Document itself
-//10- Doctype
+// //1- Element
+// //2- Attribute(DEP)
+// //3- Text Node
+// //8- Comment
+// //9- Document itself
+// //10- Doctype
 
 
 
-//Get children element nodes
+// //Get children element nodes
 // val = list.children;
 
-console.log(val)
+// val = list.children[1];
+// list.children[1].textContent='Hello';
+
+// //children of children
+// list.children[3].children[0].id ='test-link';
+// val = list.children[3].children;
+
+// val = list.firstChild;
+// val = list.firstElementChild;
+
+// val = list.lastChild;
+// val = list.lastElementChild;
+
+// //Count child elements
+// val = list.childElementCount;
+
+// //Get parent node
+// val = listItem.parentNode;
+// val = listItem.parentElement;
+// val = listItem.parentElement.parentElement;
+
+// //Get next sibling
+// val = listItem.nextSibling;
+// val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
+
+// // //Get prev sibling
+// val = listItem.previousSibling;
+// val = listItem.previousElementSibling;
+
+
+// console.log(val)
+
+// //Create Element
+
+// const li = document.createElement('li');
+
+// //Add class
+// li.className = 'collection-item';
+
+// //add ID
+// li.id = 'new-item';
+
+// //add attribute
+// li.setAttribute('title', 'New Item');
+
+// //Create text node and append
+// li.appendChild(document.createTextNode('HELLO WORLD'));
+
+// //Create new link element
+// const link = document.createElement('a');
+// //Add classes
+// link.className = 'delete-item secondary-content';
+// //Add icon html
+// link.innerHTML ='<i class="fa fa-remove"></i>';
+// //Append Link
+// li.appendChild(link);
+// //append li as child to ul
+// document.querySelector('ul.collection').appendChild(li);
+
+
+
+// console.log(li);
