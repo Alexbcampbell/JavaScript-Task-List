cc8fb5cd484a3cf65a551534eb437876204ede6e
alex Campbell <alexbe.campbell@gmail.com>
2022-05-04 01:12:41 -0500

    local and session storage pracice

--- app.js
+++ app.js
@@ -401,15 +401,65 @@ taskInput.value = '';
 
 // delItem.addEventListener('click', deleteItem)
 
-document.body.addEventListener('click', deleteItem);
+// document.body.addEventListener('click', deleteItem);
 
-function deleteItem(e){
-    // console.log(e.target)
-    // if(e.target.parentElement.className === 'delete-item secondary-content'){
-    //     console.log('delete item');
-    // }
-    if(e.target.parentElement.classList.contains('delete-item')){
-        console.log('delete item');
-        e.target.parentElement.parentElement.remove();
+// function deleteItem(e){
+//     // console.log(e.target)
+//     // if(e.target.parentElement.className === 'delete-item secondary-content'){
+//     //     console.log('delete item');
+//     // }
+//     if(e.target.parentElement.classList.contains('delete-item')){
+//         console.log('delete item');
+//         e.target.parentElement.parentElement.remove();
+//     }
+// }
+
+
+//local & session storage
+//local storage will stay until manually cleared out. Session storage will leave when browser is closed
+
+//set local storage item
+
+// localStorage.setItem('name', 'John');
+
+// //set session storage
+// sessionStorage.setItem('name', 'Beth')
+
+// //remove
+// localStorage.removeItem('name')
+
+// //get from storage
+// const name = localStorage.getItem('name')
+// const age = localStorage.getItem('age')
+
+// //clear local
+// localStorage.clear();
+// console.log(name,age);
+
+document.querySelector('form').addEventListener('submit', 
+function(e){
+    const task = document.getElementById('task').value;
+
+    let tasks;
+
+    if(localStorage.getItem('tasks') === null){
+    tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
     }
+
+    tasks.push(task);
+    
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    alert('task saved')
+
+    e.preventDefault();
 }
+
+)
+
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+tasks.forEach(function(task){
+    console.log(task)
+})
