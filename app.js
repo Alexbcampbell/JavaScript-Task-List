52bc8bbf5b05e58d29a519d4d62b5b18d07304de
alex Campbell <alexbe.campbell@gmail.com>
2022-05-04 00:54:36 -0500

    Event Bubbling and Delegation practice

--- app.js
+++ app.js
@@ -361,14 +361,55 @@ taskInput.value = '';
 // taskInput.addEventListener('input', runEvent);
 
 
-function runEvent(e){
-    console.log(`EVENT TYPE: ${e.type}`)
+// function runEvent(e){
+//     console.log(`EVENT TYPE: ${e.type}`)
 
-    console.log(e.target.value);
+//     console.log(e.target.value);
 
-    // heading.innerText = e.target.value;
-    // //get input value
-    // console.log(taskInput.value);
+//     // heading.innerText = e.target.value;
+//     // //get input value
+//     // console.log(taskInput.value);
 
-    // e.preventDefault();
+//     // e.preventDefault();
+// }
+
+//Event Bubbling and Delegation
+
+// //event bubbling
+// document.querySelector('.card-title').addEventListener('click', 
+// function(){
+//     console.log('card title')
+// })
+
+// document.querySelector('.card-content').addEventListener('click', 
+// function(){
+//     console.log('card content')
+// })
+
+// document.querySelector('.card').addEventListener('click', 
+// function(){
+//     console.log('card')
+// })
+
+// document.querySelector('.col').addEventListener('click', 
+// function(){
+//     console.log('col')
+// })
+
+//event delegation
+// const delItem = document.querySelector('.delete-item');
+
+// delItem.addEventListener('click', deleteItem)
+
+document.body.addEventListener('click', deleteItem);
+
+function deleteItem(e){
+    // console.log(e.target)
+    // if(e.target.parentElement.className === 'delete-item secondary-content'){
+    //     console.log('delete item');
+    // }
+    if(e.target.parentElement.classList.contains('delete-item')){
+        console.log('delete item');
+        e.target.parentElement.parentElement.remove();
+    }
 }
