26bd17f0886b226362c62ba2e20c4be6930fa160
alex Campbell <alexbe.campbell@gmail.com>
2022-05-10 00:12:37 -0500

    defined form/button variables, added function to add tasks, alert if no task is added and fixed ID on html

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-//JavaScript file
+//Define UI Variables
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+//Load all event listeners
+loadEventListeners();
+
+//Load all event listeners
+function loadEventListeners() {
+    //add task event
+    form.addEventListener('submit', addTask);
+}
+
+//Add Task
+function addTask(e){
+    if(taskInput.value === ''){
+        alert('Add a task');
+    }
+    e.preventDefault();
+}
