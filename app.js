9dfe6d1e0f2402a45395415c24be8027647805a8
alex Campbell <alexbe.campbell@gmail.com>
2022-05-11 00:16:19 -0500

    added event listeners and functions for remove, clear and filter tasks

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ loadEventListeners();
 function loadEventListeners() {
     //add task event
     form.addEventListener('submit', addTask);
+    //Remove task event
+    taskList.addEventListener('click', removeTask);
+    //clear task event
+    clearBtn.addEventListener('click', clearTasks);
+    //filter tasks
+    filter.addEventListener('keyup', filterTasks);
 }
 
 //Add Task
@@ -45,4 +51,37 @@ function addTask(e){
     console.log(li)
 
     e.preventDefault();
+}
+
+//remove task
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure?')){ e.target.parentElement.parentElement.remove();
+    }
+    }
+}
+
+function clearTasks(){
+    //taskList.innerHTML = '';
+
+    //faster
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild)
+    }
+}
+
+function filterTasks(e){
+const text = e.target.value.toLowerCase();
+
+document.querySelectorAll('.collection-item').forEach(
+    function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != 1){
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    }
+);
+
 }
